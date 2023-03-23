94512ad8f00d227f287d27be008476668ca03633
FuryMartin <41051953+FuryMartin@users.noreply.github.com>
2023-03-23 23:48:07 +0800

    Create WSL2-Port-Forward.bat
    Add main bat

---
+++ WSL2-Port-Forward.bat
@@ -0,0 +1,13 @@
+set WSL2_NAME=Debian
+
+set LISTEN_PORT=24
+
+wsl -d %WSL2_NAME% -u root -e /etc/init.d/ssh start
+
+netsh interface portproxy delete v4tov4 listenport=%LISTEN_PORT% listenaddress=0.0.0.0
+
+for /f "usebackq tokens=1" %%g in (`wsl -d %WSL2_NAME% hostname -I`) do set WSL2_IP_Address=%%g
+
+netsh interface portproxy add v4tov4 listenport=%LISTEN_PORT% listenaddress=0.0.0.0 connectport=22 connectaddress=%WSL2_IP_Address%
+
+pause
